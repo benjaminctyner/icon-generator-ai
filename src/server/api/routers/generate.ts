b5e7e174ca1b5bfc10152895ead0ffe927248138
Ben <benjaminctyner@gmail.com>
2023-06-05 11:19:42 -0400

    Decremented user credits.

--- src/server/api/routers/generate.ts
+++ src/server/api/routers/generate.ts
@@ -1,15 +1,46 @@
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
 import { z } from "zod";
+import { TRPCError } from "@trpc/server";
 
 export const generateRouter = createTRPCRouter({
-  generateIcon: publicProcedure
+  generateIcon: protectedProcedure
     .input(
       z.object({
         prompt: z.string(),
       })
     )
-    .mutation(({ ctx, input }) => {
+    .mutation(async ({ ctx, input }) => {
       console.log("we are here", input.prompt);
+
+      //verify that user has enough credits
+      //do an update in prisma, ctx
+      const { count } = await ctx.prisma.user.updateMany({
+        where: {
+          id: ctx.session.user.id,
+          credits: {
+            gte: 1,
+          },
+        },
+        data: {
+          credits: {
+            decrement: 1,
+          },
+        },
+      });
+
+      console.log(count);
+
+      if (count <= 0) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "You do not have enough credits.",
+        });
+      }
+
       return {
         message: "success",
       };
