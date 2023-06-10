4864fc330e826b700e5293c6bd1423c31c59bf42
Ben <benjaminctyner@gmail.com>
2023-06-10 13:34:07 -0400

    multiple icons per gen.

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -4,11 +4,13 @@ import { signIn, signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
 import { Button } from "./Button";
 import { useBuyCredits } from "~/hooks/useBuyCredits";
+import { api } from "~/utils/api";
 
 export function Header() {
   const session = useSession();
   const isLoggedin = !!session.data;
   const { buyCredits } = useBuyCredits();
+  const credits = api.user.getCredits.useQuery();
 
   return (
     <header className="container  mx-auto flex h-16 items-center justify-between bg-gray-800 px-4 ">
@@ -24,10 +26,16 @@ export function Header() {
             </PrimaryLink>
           </li>
         )}
+        <li>
+          <PrimaryLink className="flex gap-4" href="/community">
+            Community
+          </PrimaryLink>
+        </li>
       </ul>
-      <ul className="flex gap-4">
+      <ul className="flex items-center gap-4">
         {isLoggedin ? (
           <>
+            Credits Remaining {credits.data}
             <li>
               <>
                 <Button
--- src/server/api/routers/generate.ts
+++ src/server/api/routers/generate.ts
@@ -1,47 +1,38 @@
-import {
-  createTRPCRouter,
-  publicProcedure,
-  protectedProcedure,
-} from "~/server/api/trpc";
-import { z } from "zod";
 import { TRPCError } from "@trpc/server";
-import { env } from "~/env.mjs";
+import { z } from "zod";
 
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { Configuration, OpenAIApi } from "openai";
-import { COLLECTION_FORMATS } from "openai/dist/base";
+import { env } from "~/env.mjs";
 import { b64Image } from "~/data/b64Image";
 import AWS from "aws-sdk";
 
-const BUCKET_NAME = "ai-icon-generator-bct";
-
 const s3 = new AWS.S3({
   credentials: {
-    accessKeyId: process.env.ACCESS_KEY_ID ?? "error",
-    secretAccessKey: process.env.SECRET_ACCESS_KEY ?? "error",
+    accessKeyId: env.ACCESS_KEY_ID,
+    secretAccessKey: env.SECRET_ACCESS_KEY,
   },
   region: "us-east-1",
 });
 
+const BUCKET_NAME = "ai-icon-generator-bct";
+
 const configuration = new Configuration({
-  apiKey: process.env.DALLE_API_KEY ?? "error",
+  apiKey: env.DALLE_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-async function generateIcon(prompt: string): Promise<string | undefined> {
-  if (process.env.MOCK_DALLE === "true") {
-    return b64Image;
+async function generateIcon(prompt: string, numberOfIcons = 1) {
+  if (env.MOCK_DALLE === "true") {
+    return new Array<string>(numberOfIcons).fill(b64Image);
   } else {
     const response = await openai.createImage({
       prompt,
-      n: 1,
+      n: numberOfIcons,
       size: "512x512",
       response_format: "b64_json",
     });
-    // console.log("----");
-    // console.log(response.data.data[0]?.b64_json);
-    // console.log("----");
-
-    return response.data.data[0]?.b64_json;
+    return response.data.data.map((result) => result.b64_json || "");
   }
 }
 
@@ -51,62 +42,65 @@ export const generateRouter = createTRPCRouter({
       z.object({
         prompt: z.string(),
         color: z.string(),
+        shape: z.string(),
+        style: z.string(),
+        numberOfIcons: z.number().min(1).max(10),
       })
     )
     .mutation(async ({ ctx, input }) => {
-      console.log("we are here", input.prompt);
-
-      //verify that user has enough credits
-      //do an update in prisma, ctx
       const { count } = await ctx.prisma.user.updateMany({
         where: {
           id: ctx.session.user.id,
           credits: {
-            gte: 1,
+            gte: input.numberOfIcons,
           },
         },
         data: {
           credits: {
-            decrement: 1,
+            decrement: input.numberOfIcons,
           },
         },
       });
 
-      console.log(count);
-
       if (count <= 0) {
         throw new TRPCError({
           code: "BAD_REQUEST",
-          message: "You do not have enough credits.",
+          message: "you do not have enough credits",
         });
       }
 
-      // const response = await openai.createImage({
-      //   prompt: input.prompt,
-      //   n: 1,
-      //   size: "1024x1024",
-      // });
+      const finalPrompt = `a modern ${input.shape} icon in ${input.color} of ${input.prompt}, ${input.style}, minimialistic, high quality, trending on art station, unreal engine graphics quality`;
 
-      const finalPrompt = `a modern icon in ${input.color} of a ${input.prompt}, 3D rendered, metallic material, and minimalistic.`;
-      const base64Data = await generateIcon(finalPrompt);
-      //TODO save the images to the s3 bucket
+      const base64EncodedImages = await generateIcon(
+        finalPrompt,
+        input.numberOfIcons
+      );
 
-      const icon = await ctx.prisma.icon.create({
-        data: { prompt: input.prompt, userId: ctx.session.user.id },
-      });
-
-      await s3
-        .putObject({
-          Bucket: BUCKET_NAME,
-          Body: Buffer.from(base64Data!, "base64"),
-          Key: icon.id,
-          ContentEncoding: "base64",
-          ContentType: "image/png",
+      const createdIcons = await Promise.all(
+        base64EncodedImages.map(async (image) => {
+          const icon = await ctx.prisma.icon.create({
+            data: {
+              prompt: input.prompt,
+              userId: ctx.session.user.id,
+            },
+          });
+          await s3
+            .putObject({
+              Bucket: BUCKET_NAME,
+              Body: Buffer.from(image, "base64"),
+              Key: icon.id,
+              ContentEncoding: "base64",
+              ContentType: "image/png",
+            })
+            .promise();
+          return icon;
         })
-        .promise();
+      );
 
-      return {
-        imageUrl: `https://${BUCKET_NAME}.s3.amazonaws.com/${icon.id}`,
-      };
+      return createdIcons.map((icon) => {
+        return {
+          imageUrl: `https://${BUCKET_NAME}.s3.amazonaws.com/${icon.id}`,
+        };
+      });
     }),
 });
